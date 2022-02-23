103ac8aea29ef76858f01a5829bb3eef790722e1
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-02-23 16:13:49 -0300

    Simulando uma função

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Conta from "./Conta";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Componente de conta", () => {
   it("Exibir o saldo da conta como valor monetária", () => {
     render(<Conta saldo={1000} />);
     const saldo = screen.getByTestId("saldo-conta");
     expect(saldo.textContent).toBe("R$ 1000");
+  });
+
+  //Simulando uma função
+  it("Chama a função de realizar transação, quando o botão é clicado", () => {
+    const funcaoRealizarTransacao = jest.fn();
+    render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />);
+
+    fireEvent.click(screen.getByText("Realizar operação"));
+
+    expect(funcaoRealizarTransacao).toHaveBeenCalled();
   });
 });
