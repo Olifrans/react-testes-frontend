06211d5f66b25e000761c9b2e10b79592cdd2feb
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-02-23 09:47:19 -0300

    testes-reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import Conta from './conta/Conta';
+import Transacoes from './transacoes/Transacoes';
+import api from './api';
+
 import './App.css';
 
+export const calcularNovoSaldo = (valores, saldo) => {
+  if (valores.transacao === 'deposito') {
+    return saldo + parseInt(valores.valor)
+  } else {
+    return saldo - parseInt(valores.valor);
+  }
+}
+
 function App() {
+  const [saldo, atualizarSaldo] = useState(1000);
+  const [transacoes, atualizarTransacoes] = useState([]);
+
+  async function carregarTransacoes() {
+    const transacoes = await api.listaTransacoes();
+    atualizarTransacoes(transacoes);
+  }
+
+  async function obterSaldo() {
+    atualizarSaldo(await api.buscaSaldo());
+  }
+
+  function realizarTransacao(valores) {  
+    const novoSaldo = calcularNovoSaldo(valores, saldo);
+
+    api.atualizaSaldo(novoSaldo).catch((error) => console.error(error))
+    api.atualizaTransacoes(valores).catch((error) => console.error(error))
+    
+    atualizarSaldo(novoSaldo);
+    atualizarTransacoes([valores]);
+  }
+
+  useEffect(() => {
+    obterSaldo();
+    carregarTransacoes();
+  }, [saldo])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ITI Controle</h1>
       </header>
+
+      <Conta saldo={saldo} realizarTransacao={realizarTransacao}/>
+      <Transacoes transacoes={transacoes} />
     </div>
   );
 }
